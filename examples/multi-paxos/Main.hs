cc8b2a3ade1fddd8c108ae8a1599b68a8a47db20
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-07-09 23:27:52 +0100

    More of Paxos

--- examples/multi-paxos/Main.hs
+++ examples/multi-paxos/Main.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE ExplicitNamespaces, PatternSynonyms #-}
 module Main where
 
 import Paxos
 import System.Distributed.Interpret
-import System.Distributed.Prelude (Int, pattern Map)
+import System.Distributed.Prelude (mapFromList)
+import Prelude
 
-main = runProtos [P (paxos @Int Map)]
+main = runProtos [P (paxos @Int (mapFromList
+  [ ("inc", \s -> (s+1,"Result: " ++ show (s+1)))
+  , ("dec", \s -> (s-1,"Result: " ++ show (s-1)))
+  ]))]
 
--- examples/multi-paxos/Paxos.hs
+++ examples/multi-paxos/Paxos.hs
@@ -6,6 +6,8 @@ module Paxos where
 
 import System.Distributed.Prelude
 
+default (Int) -- Needed, specify what the default type for number literals should be (Int, Float, etc...).
+
 --------------------------------------------------------------------------------
 -- Configuration
 
@@ -19,10 +21,12 @@ data PaxosConf s cli = PaxosConf
 --------------------------------------------------------------------------------
 -- Main
 
-paxos :: Map String (st -> st) -- ^ Known operations
+paxos :: (FromCli ::: st) ~ st
+      => Map String (st -> (st, String)) -- ^ Known operations
       -> PaxosConf st FromCli  -- ^ The configuration read from the command line interface
       -> Protocol "paxos"
 paxos knownOps PaxosConf{..} = protocol @"paxos" do
+  puts "Starting paxos..."
 
   protocol @"replica" do -- A sub-protocol
 
@@ -32,23 +36,23 @@ paxos knownOps PaxosConf{..} = protocol @"paxos" do
     
     -- The index of the next slot in which the replica has not yet proposed any
     -- command, initially 1.
-    slot_in   <- new (1 :: Int)
+    slot_in   <- new 1
 
     -- The index of the next slot for which it needs to learn a decision before
     -- it can update its copy of the application state, equivalent to the
     -- state’s version number (i.e., number of updates) and initially 1.
-    slot_out  <- new (1 :: Int)
+    slot_out  <- new 1
 
     -- An initially empty set of requests that the replica has received and are
     -- not yet proposed or decided.
-    requests  <- new Set
+    requests  <- new (Set @Cmd)
 
     -- An initially empty map from slots to proposals that are currently outstanding.
-    proposals <- new Map
+    proposals <- new (Map @Int @Cmd)
 
     -- Another set of proposals that are known to have been decided (also
     -- initially empty).
-    decisions <- new Map
+    decisions <- new (Map @Int @Cmd)
 
     -- The set of leaders in the current configuration. The leaders of the
     -- initial configuration are passed as an argument to the replica.
@@ -56,20 +60,15 @@ paxos knownOps PaxosConf{..} = protocol @"paxos" do
 
     -- ToDo: Change to procedure syntax
     let
-      isReconfig(op) = case op of
-        StateOp{} -> False
-        ReconfigOp{} -> True
-
       propose() = do
-        while (slot_in < slot_out + slotWindow && size requests > (0::Int)) do
+        while (slot_in < slot_out + slotWindow && size requests > 0) do
 
           -- Reconfigure
-          maybe_reconf_decision <- lookup (slot_in - slotWindow) decisions
-          case maybe_reconf_decision of
-            Nothing -> ok
-            Just (_, _, op) ->
-              when (isReconfig(op)) do
-                leaders := op.leaders
+          reconf_decision <- lookup (slot_in - slotWindow) decisions
+          when (reconf_decision.exists) do
+            let (_, _, op) = reconf_decision.value
+            when (isReconfig(op)) do
+              leaders := op.leaders
 
           when (slot_in `notin` decisions) do
             slot_in_copy <- copy(slot_in)
@@ -81,18 +80,58 @@ paxos knownOps PaxosConf{..} = protocol @"paxos" do
           next_slot <- slot_in + (1::Int)
           slot_in := next_slot
 
-      perform((k,cid,op)) = undefined
-
-
+      perform((k::Int,cid::Int,op)) = do
+        older_decision <- decisions `randomElemWith'` (\(slot,(k'::Int,cid'::Int,op')) ->
+            slot < slot_out && k == k' && cid == cid' && op == op')
+        if (isReconfig(op) || older_decision.exists) then do
+          slot_in_val <- slot_in + (1::Int)
+          slot_in := slot_in_val
+        else do
+          operation  <- lookup op.name knownOps
+          state_copy <- copy(app_state)
+          let (next, result) = operation.value(state_copy)
+          app_state := next
+          next_slot <- slot_in + (1::Int)
+          slot_out := next_slot
+          trigger responseIndication result
 
     upon commandRequest \cmdName -> do
-
       requests += cmdName
-      call propose()
+      propose()
 
+    upon receive \DecideMsg{slot, cmd} -> do
+      decisions += (slot, cmd)
+      while (decisions `contains` slot_out) do
+        c' <- proposals ! slot_out
+        c'' <- lookup slot_out proposals
+        when (c''.exists) do
+          slot_out_val <- get(slot_out)
+          proposals -= slot_out_val
+          when (c' != c''.value) do
+            requests += c''.value
+        perform(c')
+
+      -- Finally, propose again
+      propose()
+
+  protocol @"leader" do
+
+    ok
+
+  protocol @"acceptor" do
+
+    ballot_num <- new (-1::Int)
+    accepted   <- new (Set @PValue)
+    ok
+
+    -- upon receive \P1A{} -> do
+    --   ok
+
+    -- upon receive \P2A{} -> do
+    --   ok
 
 --------------------------------------------------------------------------------
--- * Interface
+-- * Replica Interface
 
 -- A command is a triple ⟨κ,cid,operation⟩, where κ1 is the identifier of the
 -- client that issued the command and cid is a client-local unique command
@@ -100,12 +139,23 @@ paxos knownOps PaxosConf{..} = protocol @"paxos" do
 --
 -- An operation is either a string referring to a specific an operation on the
 -- state given in the known operations map, or a reconfiguration operation which carries a set of leaders.
-type Cmd = (Int, Int, String)
+type Cmd = (Int, Int, Op)
 data Op = StateOp { name :: String }
         | ReconfigOp { leaders :: Set Host }
+        deriving (Eq, Ord, Generic, Binary)
+
+isReconfig(StateOp{}) = False
+isReconfig(ReconfigOp{}) = True
 
 commandRequest = request @Cmd "Command"
+responseIndication = request @String "ClientResponse"
 
 data ProposeMsg = ProposeMsg { to :: Host, slot :: Int, cmd :: Cmd } deriving (Generic, Binary)
 data DecideMsg  = DecideMsg  { to :: Host, slot :: Int, cmd :: Cmd } deriving (Generic, Binary)
 
+--------------------------------------------------------------------------------
+-- * Acceptor Interface
+
+-- | Let a pvalue be a triple consisting of a ballot number, a slot number,
+-- and a command
+type PValue = (Int, Int, Cmd)
